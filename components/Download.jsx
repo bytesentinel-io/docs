08c212e6b4cabe8a531dc3243c6a7c41e6568e9e
Björn Benouarets <70456959+bbenouarets@users.noreply.github.com>
2023-03-20 13:45:34 +0100

    Add overview of providers

--- components/Download.jsx
+++
@@ -1,9 +0,0 @@
-import { ImDownload } from "react-icons/im";
-
-export default function Download({ title, link, text }) {
-    return (
-        <div className="pt-5">
-            <a href={link} className="bg-primary text-white px-4 py-3 rounded-xl">{title}</a>
-        </div>
-    );
-}
