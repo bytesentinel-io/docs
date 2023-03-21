998bb1de3ee6bbc2730b38b2192d513f0594404c
Björn Benouarets <70456959+bbenouarets@users.noreply.github.com>
2023-03-21 18:27:56 +0100

    Update ByteSentinel favicons

--- theme.config.jsx
+++ theme.config.jsx
@@ -54,7 +54,7 @@ export default {
         <link rel="apple-touch-icon" sizes="180x180" href="icons/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="icons/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="icons/favicon-16x16.png" />
-        <link rel="manifest" href="icons/site.webmanifest" />
+        <link rel="manifest" href="site.webmanifest" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
     </>,
